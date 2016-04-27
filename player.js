011b8a6486925cd4ff143ef53fc7ba2715bc477f
fatmatto <cikkense@gmail.com>
2016-04-27 19:41:43 +0200

    added allin

--- player.js
+++ player.js
@@ -26,11 +26,16 @@ exports = module.exports = {
     var big_blind = 2*gamestate.sb;
 
     var our_bet = 3*big_blind;
+
+    var our_index = gamestate.me;
+    var our_chips = gamestate.players[our_index].chips;
     // 
     // Se fanno call al nostro bet (no rilancio)
-    // puntiamo i 3/4 del pot
+    // puntiamo i 2/4 del pot
     //Se rilanciano controrilancio all in
     //
+    //se preflop rilanciano maggiore di noi punta tutto in allin
+    //se flop puntano più dei nostri 2/pot punta tutte le nostre chips 
     var doesSomeoneBet = function(players) {
       return players.some(function(x){
         return x.chipsBet >= our_bet;
@@ -42,8 +47,20 @@ exports = module.exports = {
     }
 
 
+    var siamoInPreFlop = function() {
+      return gamestate.commonCards.length === 0;
+    }
+
+    var hannoPuntatoPiuDiNoi = function() {
+      return our_bet < gamestate.callAmount;
+    }
+
+    if (siamoInPreFlop() && hannoPuntatoPiuDiNoi())
+      return our_chips;
+
+
     if (doesSomeoneBet() && siamoInFlop())
-    return (2*the_pot)/4;    
+      return (2*the_pot)/4;    
 
 
     previous_bet = our_bet;
